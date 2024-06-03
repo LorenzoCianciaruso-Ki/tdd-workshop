84e75bea4c552c681e00bb433d1ba12e0bf3afce
Lorenzo Cianciaruso <lorenzo.cianciaruso@ki-insurance.com>
2024-06-03 14:59:05 +0100

    simple function tested

--- tdd-tutorial/src/examples/arrayCalculator.test.ts
+++ tdd-tutorial/src/examples/arrayCalculator.test.ts
@@ -1,7 +1,50 @@
 import { expect, test } from 'vitest'
 import ArrayCalculator from './arrayCalculator'
 
-test('first test', () => {
+test('should return 0 when parameter is empty array', () => {
     const calculator = new ArrayCalculator();
-    expect(calculator).toBeFalsy()
-}) 
+
+    const total = calculator.sum([]);
+
+    expect(total).toBe(0);
+});
+
+test('should return the first element when array contains one element', () => {
+    const calculator = new ArrayCalculator();
+
+    const total = calculator.sum([5]);
+
+    expect(total).toBe(5);
+})
+
+test('should sum up all the values in the array', () => {
+    const calculator = new ArrayCalculator();
+
+    const total = calculator.sum([4, 8]);
+
+    expect(total).toBe(12);
+});
+
+test('should sum up all the values in the array', () => {
+    const calculator = new ArrayCalculator();
+
+    const total = calculator.sum([4, 8, 1, 4, 6]);
+
+    expect(total).toBe(23);
+});
+
+test('test negative numbers', () => {
+    const calculator = new ArrayCalculator();
+
+    const total = calculator.sum([3, -5, 3, -2 , -1, 6]);
+
+    expect(total).toBe(4);
+});
+
+test('test decimal numbers', () => {
+    const calculator = new ArrayCalculator();
+
+    const total = calculator.sum([1.4, -0.4, 1.8]);
+
+    expect(total).toBe(2.8);
+});
--- tdd-tutorial/src/examples/arrayCalculator.ts
+++ tdd-tutorial/src/examples/arrayCalculator.ts
@@ -1,3 +1,16 @@
 export default class ArrayCalculator {
+    sum(params: number[]) {
+        return params.reduce((previous, current) => (previous + current), 0);
+    }
 
+    // Test 1
+    // 0
+
+    // Test 2 - [5]
+    // 0 (previous) + 5 (current) = 5
+
+    // Test 3 - [4, 8]
+    // 0
+    // 0 (previous) + 4 (current) = 4
+    // 4 (previous) + 8 (current) = 12
 }
